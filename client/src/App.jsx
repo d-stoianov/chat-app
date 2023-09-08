823569ddd569cb0c3069a52c054588356f1d43fb
dmitriy <dima22022005@gmail.com>
2023-09-09 00:14:35 +0200

    Initial commit.

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-xl flex justify-center">
+                Hello chat !
+            </h1>
+        </div>
+    )
+}
+
+export default App
