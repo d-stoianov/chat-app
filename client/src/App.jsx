a7696a33d43bc62548c6819945c40dfc738c117b
dmitriy <dima22022005@gmail.com>
2023-09-09 00:32:44 +0200

    Create Header.jsx component.
    Also add global purple background.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header"
+
 const App = () => {
     return (
         <div>
-            <h1 className="text-xl flex justify-center">
-                Hello chat !
-            </h1>
+            <Header />
         </div>
     )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="bg-zinc-900 w-full h-16 md:h-20 z-100 flex justify-center items-center">
+            <h1 className="text-zinc-200 font-bold">
+                Let's chat ! 💭
+            </h1>
+        </header>
+    )
+}
+
+export default Header
