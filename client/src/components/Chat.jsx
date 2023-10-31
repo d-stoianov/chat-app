e1c3eb6d87268d16a0a3a95274e701f4422a1ce4
Dmytro <dima22022005@gmail.com>
2023-10-31 20:31:18 +0100

    Implement scroll to the bottom of the chat if new messages appear.

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,10 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import ChatInput from "./ChatInput"
 import Message from "./Message"
 import service from "../services/service"
 
 const Chat = ({ username }) => {
     const [messages, setMessages] = useState([])
+    const messagesEndRef = useRef(null)
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    const renderMessage = async (message) => {
+        await service.sendMessage(message)
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages])
 
     useEffect(() => {
         service.getMessages().then((data) => setMessages(data))
@@ -16,16 +29,13 @@ const Chat = ({ username }) => {
         return () => service.removeReceiveMessageListener()
     }, [])
 
-    const renderMessage = async (message) => {
-        await service.sendMessage(message)
-    }
-
     return (
         <main className="w-full h-full flex flex-col justify-between overflow-y-hidden">
             <div className="w-full px-4 py-1 mt-2 flex flex-col overflow-y-auto">
                 {messages.map((msg) => (
                     <Message key={msg._id} username={username} message={msg} />
                 ))}
+                <div ref={messagesEndRef} />
             </div>
             <ChatInput onMessage={renderMessage} />
         </main>
