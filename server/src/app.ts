dbdc4534a8a3a3292e062d5902fa04e53aef30df
Dmytro <dmytrostoianov05@gmail.com>
2024-11-19 00:27:14 +0100

    Create http server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,14 @@
-console.log("testing")
+import * as http from 'http'
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+
+    res.write('server is live')
+    res.end()
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port - ${PORT}`)
+})
