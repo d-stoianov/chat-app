bbeae50f6edfd5a1333c597c981d707d42ab3d2b
Dmytro <dmytrostoianov05@gmail.com>
2024-11-19 01:04:28 +0100

    Create basic page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
 const App = () => {
-    return <h1 className="text-2xl font-bold text-red-500">test</h1>
+    return (
+        <main className="flex h-full w-full flex-col items-center bg-slate-800">
+            <form
+                className="mt-36 flex w-full flex-col items-center gap-2 px-8 sm:w-[22rem]"
+                onSubmit={(e) => e.preventDefault()}
+            >
+                <input
+                    className="w-full rounded-2xl p-2 outline-none"
+                    type="text"
+                    placeholder="name"
+                />
+                <button className="w-full rounded-2xl bg-white p-2 outline-none hover:bg-gray-100">
+                    chat!
+                </button>
+            </form>
+        </main>
+    )
 }
 
 export default App
